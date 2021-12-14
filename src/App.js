9cdfcf1bc036e86fa80b8a08b5d3b3136328fd57
Austin <austinchoi22@gmail.com>
2021-12-13 21:56:34 -0700

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { CenterDiv } from './components/Styles';
+// import SinglePlayer from './pages/PlayerOne';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CenterDiv>
+      <h1>Rock, Paper, Scissors</h1>
+      {/* <PlayerOne /> */}
+    </CenterDiv>
   );
 }
 
